86f16fa4a1e015db1acd3ef1a2f0005ca7af0d64
Lucas <lliu0060@student.monash.edu>
2024-08-23 16:10:22 +1000

    update for detector docs

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ synopsis:
 - be able to navigate to a collection box located in the center of the four quadrants of the court
 - be able to deposit collected tennis balls in the cardboard collection box of dimensions 60x45x18cm
 - may travel into other quadrants of the court momentarily
+
+## Pi Setup
+
+Environment initialisation:
+
+```sh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y git libgl1
+```
+
+To install Python packages:
+
+```sh
+python3.11 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+```
+
--- camera/detector.py
+++ camera/detector.py
@@ -5,16 +5,15 @@ from copy import deepcopy
 from ultralytics import YOLO
 from ultralytics.utils import ops
 import utils
+import time
+
 
 class Detector:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
 
-        self.class_colour = {
-            'Balls': (64, 255, 255)
-        }
+        self.class_colour = {"Balls": (64, 255, 255)}
 
-        
     def detect_img(self, img):
         bboxes = self.get_bboxes(img)
         img_out = deepcopy(img)
@@ -27,13 +26,22 @@ class Detector:
             y2 = int(xyxy[3])
 
             # draw bounding box
-            img_out = cv2.rectangle(img_out, (x1, y1), (x2, y2), self.class_colour[bbox[0]], thickness=2)
+            img_out = cv2.rectangle(
+                img_out, (x1, y1), (x2, y2), self.class_colour[bbox[0]], thickness=2
+            )
 
             # draw class label
-            img_out = cv2.putText(img_out, bbox[0], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                  self.class_colour[bbox[0]], 2)
+            img_out = cv2.putText(
+                img_out,
+                bbox[0],
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                self.class_colour[bbox[0]],
+                2,
+            )
         return bboxes, img_out
-    
+
     def get_bboxes(self, cv_img):
         preds = self.model.predict(cv_img, imgsz=320, verbose=False)
 
@@ -50,19 +58,25 @@ class Detector:
                 bboxes.append([p.names[int(box_label)], np.asarray(b_cord)])
 
         return bboxes
-    
+
     # testing
-    if __name__ == '__main__':
+    if __name__ == "__main__":
         script_dir = os.path.dirname(os.path.abspath(__file__))
         from detector import Detector
-        yolo = Detector(f'{script_dir}/model.pt')
+
+        yolo = Detector(f"{script_dir}/model.pt")
 
         while True:
+            last_time = time.time()
             img = utils.get_frame()
             bboxes, img_out = yolo.detect_img(img)
             print(bboxes)
-            print(len(bboxes))
+            # print(len(bboxes))
 
-            cv2.imshow('yolo detect', img_out)
+            frame_time = time.time() - last_time
+            print(f"frame time: {time.time()-last_time}")
+            # cv2.imshow('yolo detect', img_out)
+            cv2.imwrite(f"test/{time.time()}.png", img_out)
 
-            if cv2.waitKey(1) == ord('q'): break
+            if cv2.waitKey(1) == ord("q"):
+                break
